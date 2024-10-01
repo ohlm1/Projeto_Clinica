e371c2f5181a9554e35c68231ff6c4312521e38f
Derik Belchior <contatoderiktorres@gmail.com>
2024-09-30 21:20:38 -0300

    requirements added

--- app/config.py
+++ app/config.py
@@ -4,10 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 class Config:
-    # SECRET_KEY = ''
-    # SQLALCHEMY_DATABASE_URI = 'mysql://root:{SECRET_KEY}@localhost/clinica'
-    # SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
-    TESTING = True
+    SECRET_KEY = ''
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:123456@localhost/clinica'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+    # TESTING = True
 
 
